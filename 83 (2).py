b9b303051a7a234aa74cbba1309878355fa6a944
BEERAMPAVANI <2311cs020083@mallareddyuniversity.ac.in>
2025-01-07 09:52:43 +0530

    Add files via upload

---
+++ 83 (2).py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+# Create a list with 5 elements
+my_list = [10, 20, 30, 40, 50]
+print(my_list[0])   
+print(my_list[2])   
+print(my_list[-1])  
+print(my_list[1:4])   
+
+
+# In[6]:
+
+
+# Create a tuple with 5 elements
+my_tuple = (100, 200, 300, 400, 500)
+print(my_tuple[1])   
+print(my_tuple[-2])  
+print(my_tuple[2:5])   
+
+
+# In[7]:
+
+
+# Create a dictionary with 5 key-value pairs
+my_dict = {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5}
+print(my_dict['a'])   
+print(my_dict['c'])
+print(my_dict.get('e'))  
+print(my_dict.get('f', 'Not Found'))  
+
+
+# In[ ]:
+
+
+
+
